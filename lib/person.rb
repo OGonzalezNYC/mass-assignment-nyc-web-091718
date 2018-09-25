87ed99b03c81a5dfbe86f9f4682ddaafbbafc993
OGonzalezNYC <OGonzalezNYC@Gmail.com>
2018-09-25 17:58:15 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
-  #your code here
+  #
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
+  :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(hash_attribute)
+    hash_attribute.each do |key, value|
+      self.send(("#{key}="), value)
+
+    #hash_attribute.send do |key, value|
+
+    end
+
+  end
 end
